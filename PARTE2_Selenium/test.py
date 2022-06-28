c966ae90a73a94fcad0d0806280d11c91dabac7b
Juan <jsilvalu93@gmail.com>
2022-06-28 17:43:22 +0200

    Part2_V1_Complete

--- PARTE2_Selenium/test.py
+++ PARTE2_Selenium/test.py
@@ -3,13 +3,16 @@
 import json
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+
 
 class TestClass:
 
     @pytest.fixture(autouse=True)
     def driver_setup_and_teardown(self):
-        self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         self.driver.maximize_window()
         self.driver.implicitly_wait(15)
         self.driver.get("https://www.cheesecake.com/")
@@ -18,8 +21,13 @@ class TestClass:
         self.driver.quit()
         print("\n-----TEST DONE-----")
 
-    @pytest.mark.parametrize("subtype", get_json_subtypes_list("car_accident_3_party"))
-    @pytest.mark.skipif(True, reason="Test skipped by user")
-    def test_car_accident_3_party(self, subtype):
+    @pytest.mark.skipif(False, reason="Test skipped by user")
+    def test1(self):
         driver = self.driver
-        driver.find
+        driver.find_element(By.ID, "ctl00_Header1_ctl00_KeywordField").send_keys("strawberry")
+        driver.find_element(By.ID, "ctl00_Header1_ctl00_btnSearch").click()
+        product_list = driver.find_element(By.CSS_SELECTOR, ".productgrid.table").find_elements(By.CLASS_NAME, "SingleProductDisplayPanel")
+        assert len(product_list) == 9
+
+        price4_element = product_list[3].find_element(By.ID, "ctl00_MainContentHolder_ProductGridDisplay_rpProductGrid_ctl04_SingleProductDisplay_PriceLabel").text
+        assert price4_element == "$44.99"
